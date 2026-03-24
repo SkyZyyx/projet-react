3d3ce01b9dc8ab910ed3a07c73d0b27cbc1adee4
SkyZyyx <SkyZyyx@users.noreply.github.com>
2026-03-24 17:23:59 +0100

    first push

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './index.css'
+function App() {
+
+  return (
+    <div className=''>
+
+      <h1 className="bg-red-500 text-white p-4">Welcome to React with TypeScript!</h1>
+    </div>
+    
+  )
+}
+
+export default App
